fb7ad8a4bc44b2b009118c6cc0848039735f3529
Santino Ventrice <santino.ventrice@gmail.com>
2025-09-28 02:49:33 -0300

    Removing default Next Template
    Update page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins, Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  weight: ["400", "700"],
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
   subsets: ["latin"],
 });
 
@@ -24,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${poppins.className} ${inter.className}`}>
         {children}
       </body>
     </html>
