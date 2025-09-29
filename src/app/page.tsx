5d20c03751090ffd21ee5e2012dc035a8b144ffb
Santino Ventrice <santino.ventrice@gmail.com>
2025-09-29 02:25:49 -0300

    Adding About me section component to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@
 
 // Sections
 import HeroSection from "@/components/sections/hero-section/hero-section";
+import AboutMeSection from "@/components/sections/about-me/aboutme-section";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <AboutMeSection />
     </main>
   );
 }
