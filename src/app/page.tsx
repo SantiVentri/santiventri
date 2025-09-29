b23ff0f378460835bacb148e4e6683ee5c47d278
Santino Ventrice <santino.ventrice@gmail.com>
2025-09-29 02:49:10 -0300

    Adding Skills section to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,15 @@
 
 // Sections
 import HeroSection from "@/components/sections/hero-section/hero-section";
-import AboutMeSection from "@/components/sections/about-me/aboutme-section";
+import AboutMeSection from "@/components/sections/aboutme-section/aboutme-section";
+import SkillsSection from "@/components/sections/skills-section/skills-section";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <AboutMeSection />
+      <SkillsSection />
     </main>
   );
 }
