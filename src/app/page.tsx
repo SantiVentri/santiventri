dde04de184b3b61bbd03ff29c50d3bc76ffb762c
Santino Ventrice <santino.ventrice@gmail.com>
2025-10-04 02:59:25 -0300

    Adding CTA section to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import HeroSection from "@/components/sections/hero-section/hero-section";
 import AboutMeSection from "@/components/sections/aboutme-section/aboutme-section";
 import SkillsSection from "@/components/sections/skills-section/skills-section";
 import ProjectsSection from "@/components/sections/projects-section/projects-section";
+import CTASection from "@/components/sections/cta-section/cta-section";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <AboutMeSection />
       <SkillsSection />
       <ProjectsSection />
+      <CTASection />
     </main>
   );
 }
