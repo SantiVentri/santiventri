cbbecdab8823cbad4a8d0eb9c94e649ebd395669
Santino Ventrice <santino.ventrice@gmail.com>
2025-10-04 02:57:30 -0300

    Adding Projects section to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@
 import HeroSection from "@/components/sections/hero-section/hero-section";
 import AboutMeSection from "@/components/sections/aboutme-section/aboutme-section";
 import SkillsSection from "@/components/sections/skills-section/skills-section";
+import ProjectsSection from "@/components/sections/projects-section/projects-section";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <HeroSection />
       <AboutMeSection />
       <SkillsSection />
+      <ProjectsSection />
     </main>
   );
 }
