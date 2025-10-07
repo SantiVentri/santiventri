089f1ce6ab92c60e97060d92689f2e250ad592ec
Santino Ventrice <santino.ventrice@gmail.com>
2025-10-07 19:42:17 -0300

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-// Styles
-// import styles from "./page.module.css";
-
-// Sections
 import HeroSection from "@/components/sections/hero-section/hero-section";
 import AboutMeSection from "@/components/sections/aboutme-section/aboutme-section";
 import SkillsSection from "@/components/sections/skills-section/skills-section";
